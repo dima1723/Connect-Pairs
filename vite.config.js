6e6af14cb9ba92632ea8fea64013ae2255687698
dima1723 <you@example.comdimon2002223@gmail.com>
2024-07-13 18:53:46 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: `/ConnectPairs/`,
+  publicPath: `/Connect-Pairs/`,
   plugins: [
     vue(),
   ],
